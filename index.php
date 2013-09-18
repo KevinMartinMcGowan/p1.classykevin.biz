fe37c64dbe46e2f2dfa720611737cd89e15dd3a6
Kevin McGowan <kevinmartinmcgowan@gmail.com>
2013-09-17 20:51:48 -0400

    rough draft of page

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-Welcome to p1  :-)
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+		
+		<title>Kevin McGowan's 1st Project</title>
+		
+		<link rel="stylesheet" type="text/css" href="style.css">
+		
+		<!--include jquery & backstretch-->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="jquery.backstretch.min.js"></script>
+		<script type="text/javascript">
+			$.backstretch("placeholder.jpg", {speed: 150});
+		</script>
+		
+	</head>
+
+	<body>
+		
+		<div id="info">
+			<h2>Kevin McGowan</h2>
+			<p class="i">uses TextWrangler. It makes him feel like a cowboy!</p>
+			<p>Kevin is an independent filmmaker who lives Cambridge, Massachusetts.  His 		films have been screened in numerous festivals and his short documentary, <a href="http://vimeo.com/6712822">Stone Mountain</a>, was broadcast on PBS.  He's worked as an editor, HDSLR Shooter, and director on documentaries, low-budget feature films, and music videos.  <a href="https://vimeo.com/album/2082152">Some of Kevin's movies can be viewed here.</a>  Kevin is a history buff, avid collector of laserdiscs, an unapologetic A/V geek. <a href="http://kevinmartinmcgown.tumblr.com/">Check out what Kevin is up to here.</p>
+			<br />
+			
+		</div>
+
+	</body>
+	
+</html>
