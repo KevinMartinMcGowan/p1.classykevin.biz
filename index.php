7587895e2a8332859cb14a4d6d7ae543d98cf4fd
Kevin McGowan <kevinmartinmcgowan@gmail.com>
2013-09-17 21:44:58 -0400

    Updated copy and images

--- index.php
+++ index.php
@@ -7,25 +7,46 @@
 		
 		<link rel="stylesheet" type="text/css" href="style.css">
 		
-		<!--include jquery & backstretch-->
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-		<script type="text/javascript" src="jquery.backstretch.min.js"></script>
-		<script type="text/javascript">
-			$.backstretch("placeholder.jpg", {speed: 150});
-		</script>
 		
 	</head>
 
 	<body>
 		
-		<div id="info">
-			<h2>Kevin McGowan</h2>
-			<p class="i">uses TextWrangler. It makes him feel like a cowboy!</p>
-			<p>Kevin is an independent filmmaker who lives Cambridge, Massachusetts.  His 		films have been screened in numerous festivals and his short documentary, <a href="http://vimeo.com/6712822">Stone Mountain</a>, was broadcast on PBS.  He's worked as an editor, HDSLR Shooter, and director on documentaries, low-budget feature films, and music videos.  <a href="https://vimeo.com/album/2082152">Some of Kevin's movies can be viewed here.</a>  Kevin is a history buff, avid collector of laserdiscs, an unapologetic A/V geek. <a href="http://kevinmartinmcgown.tumblr.com/">Check out what Kevin is up to here.</p>
-			<br />
-			
-		</div>
+		<div id="wrapper">
+		
+		
+			<div id="info">
+				<h2>Kevin McGowan</h2>
+				
+				<p class="i">uses TextWrangler. It makes him feel like a cowboy!</p>
+				
+				<p>Kevin has a cat named Sam.  Sometimes Sam likes to stand on Kevin's
+				laptop.  Kevin doesn't like that.  When Kevin isn't chasing the cat away 
+				from his laptop, he is an avid photographer, filmmaker, and collector of 
+				laserdiscs.</p>  
+				
+				<p>Kevin lives in the People's Republic of Cambridge where he works.  He
+				is always on the hunt for a good cup of coffee.  Always.
+				</p>
+				
+				
+			</div>
 
+			<div id="photo">
+				<img src="sam.jpg" height="320" width="320" />
+				<br>
+				<p class="i">Sam the cat.</p>
+			</div>
+
+		
+		</div>
+<!--include jquery & backstretch-->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="jquery.backstretch.min.js"></script>
+		<script type="text/javascript">
+			$.backstretch("nutco.jpg", {speed: 150});
+		</script>
+		
 	</body>
 	
 </html>
